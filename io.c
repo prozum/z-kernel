bcf59f8efe6e78cb2727a2f2df92661f0cd88dd7
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-09-04 20:48:11 +0200

    Added 8/16/32 bit outX and inX for I/O

--- io.c
+++ io.c
@@ -1,10 +1,59 @@
+#include <stdint.h>
+
 #include "io.h"
 
-void outb(unsigned short port, unsigned char data)
+/* Send 8 bit value on I/O location. */
+void outb(uint16_t port, uint8_t val)
 {
-	asm("outb %0, %1"
-	: : "a" (data), "d" (port)
+	asm volatile ("outb %0, %1"
+	: : "a" (val), "Nd" (port)
 	);
+}
+
+/* Send 16 bit value on I/O location. */
+void outw(uint16_t port, uint16_t val)
+{
+	asm volatile ("outw %0, %1"
+	: : "a" (val), "d" (port)
+	);
+}
+
+/* Send 32 bit value on I/O location. */
+void outl(uint16_t port, uint32_t val)
+{
+	asm volatile ("outl %0, %1"
+	: : "a" (val), "d" (port)
+	);
+}
+
+/* Recieves 8 bit value from I/O location. */
+uint8_t inb(uint16_t port)
+{
+	uint8_t ret;
+	asm volatile ("inb %1, %0"
+	: "=a" (ret) : "Nd" (port)
+	);
+	return ret;
+}
+
+/* Recieves 16 bit value from I/O location. */
+uint16_t inw(uint16_t port)
+{
+	uint16_t ret;
+	asm volatile ("inw %1, %0"
+	: "=a" (ret) : "d" (port)
+	);
+	return ret;
+}
+
+/* Recieves 32 bit value from I/O location. */
+uint32_t inl(uint16_t port)
+{
+	uint32_t ret;
+	asm volatile ("inl %1, %0"
+	: "=a" (ret) : "d" (port)
+	);
+	return ret;
 }
 
 #define FB_COMMAND_PORT		0x3D4
--- kmain.c
+++ kmain.c
@@ -6,32 +6,29 @@
 /* kernel entry main */
 void kernel_entry_main()
 {
-	fb_move_cursor(800);
-/*
 	terminal_initialize();
-	terminal_writestring("         ###    #########################        \n");
-	terminal_writestring("         ###    #########################        \n");
-	terminal_writestring("         ###    #####                            \n");
-	terminal_writestring("         ###       #####                         \n");
-	terminal_writestring("         ###          #####                      \n");
-	terminal_writestring("         ###             #####                   \n");
-	terminal_writestring("         ###                #####                \n");
-	terminal_writestring("   ###############             #####             \n");
-	terminal_writestring("         ###                #####                \n");
-	terminal_writestring("         ###             #####                   \n");
-	terminal_writestring("         ###          #####                      \n");
-	terminal_writestring("         ###       #####                         \n");
-	terminal_writestring("         ###    #####                            \n");
-	terminal_writestring("         ###    #########################        \n");
-	terminal_writestring("         ###    #########################        \n");
-	terminal_writestring("                                                 \n");
-	terminal_writestring("#####       #   # ##### ###   ##  # ##### #      \n");
-	terminal_writestring("    #       #  #  #     #  #  ##  # #     #      \n");
-	terminal_writestring("   #        # #   #     #   # # # # #     #      \n");
-	terminal_writestring("  #         ##    ####  #  #  # # # ####  #      \n");
-	terminal_writestring("  #   ##### ##    #     ###   # # # #     #      \n");
-	terminal_writestring(" #          # #   #     #  #  # # # #     #      \n");
-	terminal_writestring("#           #  #  #     #   # #  ## #     #      \n");
-	terminal_writestring("#####       #   # ##### #   # #  ## ##### #####  \n");
-*/
+	terminal_writestring("                    ###    #########################        \n");
+	terminal_writestring("                    ###    #########################        \n");
+	terminal_writestring("                    ###    #####                            \n");
+	terminal_writestring("                    ###       #####                         \n");
+	terminal_writestring("                    ###          #####                      \n");
+	terminal_writestring("                    ###             #####                   \n");
+	terminal_writestring("                    ###                #####                \n");
+	terminal_writestring("              ###############             #####             \n");
+	terminal_writestring("                    ###                #####                \n");
+	terminal_writestring("                    ###             #####                   \n");
+	terminal_writestring("                    ###          #####                      \n");
+	terminal_writestring("                    ###       #####                         \n");
+	terminal_writestring("                    ###    #####                            \n");
+	terminal_writestring("                    ###    #########################        \n");
+	terminal_writestring("                    ###    #########################        \n");
+	terminal_writestring("                                                            \n");
+	terminal_writestring("           #####       #   # ##### ###   ##  # ##### #      \n");
+	terminal_writestring("               #       #  #  #     #  #  ##  # #     #      \n");
+	terminal_writestring("              #        # #   #     #   # # # # #     #      \n");
+	terminal_writestring("             #         ##    ####  #  #  # # # ####  #      \n");
+	terminal_writestring("             #   ##### ##    #     ###   # # # #     #      \n");
+	terminal_writestring("            #          # #   #     #  #  # # # #     #      \n");
+	terminal_writestring("           #           #  #  #     #   # #  ## #     #      \n");
+	terminal_writestring("           #####       #   # ##### #   # #  ## ##### #####  \n");
 }
